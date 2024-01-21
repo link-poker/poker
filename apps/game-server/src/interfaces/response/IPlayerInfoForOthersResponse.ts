dcc15ca8528235c9b291fd27c33bec9f680e132a
retocrooman <tanakatarou@eifuchishihounoMacBook-Air.local>
2024-01-21 12:33:18 +0900

    fix IPlayerInfoForOthers response type

--- apps/game-server/src/interfaces/response/IPlayerInfoForOthersResponse.ts
+++ apps/game-server/src/interfaces/response/IPlayerInfoForOthersResponse.ts
@@ -1,10 +1,8 @@
-import { CardInfo } from '../../domain/core/Card';
-
 export interface IPlayerInfoForOthersResponse {
   id: string;
   bet: number;
   raise: number | null;
-  holeCards: CardInfo[] | null;
+  holeCards: { rank: string; suit: string }[] | null;
   folded: boolean;
   showCards: boolean;
   left: boolean;
