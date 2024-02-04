2c1e306bbaaa31af5aeca3df5ecdae0163d95f53
retocrooman <tanakatarou@eifuchishihounoMacBook-Air.local>
2024-02-05 01:28:23 +0900

    add: start game button

--- apps/client/src/components/ActionContainer.tsx
+++ apps/client/src/components/ActionContainer.tsx
@@ -1,11 +1,23 @@
 import { BsCircleFill } from 'react-icons/bs';
 import { useTable } from 'hooks/useTable';
 import { useUser } from 'hooks/useUser';
+import StartActionContainer from './StartActionContainer';
 
 export default function ActionContainer() {
   const { table } = useTable();
   const { user } = useUser();
+  const isOwner = table.owner.id === user.id;
   const isYourTurn = table.poker.currentActor && table.poker.currentActor.id === user.id;
+  const isNotEnoughPlayers = table.poker.activePlayers.length < 2;
+  const isReadyStart = !table.poker.currentRound && table.poker.activePlayers.length >= 2;
+
+  if (isNotEnoughPlayers) return <></>;
+
+  if (isReadyStart) {
+    if (!isOwner) return <></>;
+    return <StartActionContainer />;
+  }
+
   return (
     <div className='flex flex-col w-[39vw] gap-3 items-end'>
       <div className='flex flex-row justify-end items-center gap-2 text-yellow-300 text-2xl'>
---
+++ apps/client/src/components/StartActionContainer.tsx
@@ -0,0 +1,25 @@
+import { useWebSocketServiceContext } from 'providers/WebSocketProvider';
+
+export default function StartActionContainer() {
+  const webSocketService = useWebSocketServiceContext();
+
+  // TODO: shuffle seats
+  // const onClickShuffleSeats = () => {
+  //   if (webSocketService) webSocketService.shuffleSeats();
+  // };
+
+  const onClickStartGame = () => {
+    if (webSocketService) webSocketService.dealCards();
+  };
+
+  return (
+    <div className='flex flex-col w-[39vw] gap-3 items-end'>
+      <div className='flex flex-row justify-end gap-2'>
+        {/* <button className='outline-white-btn rounded-xl px-6 py-6 text-xl'>SHUFFLE SEATS</button> */}
+        <button className='green-btn rounded-xl px-6 py-6 text-xl' onClick={onClickStartGame}>
+          START GAME
+        </button>
+      </div>
+    </div>
+  );
+}
