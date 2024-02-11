d897c6abae24b36d55a7be2a2a094150a1bba7be
retocrooman <tanakatarou@eifuchishihounoMacBook-Air.local>
2024-02-11 23:46:45 +0900

    feat: go home when click link poker

--- apps/client/src/components/ExternalInfoContainer.tsx
+++ apps/client/src/components/ExternalInfoContainer.tsx
@@ -1,12 +1,21 @@
 import { useUser } from 'hooks/useUser';
+import { useRouter } from 'next/navigation';
 
 export default function ExternalInfoContainer() {
   const { user } = useUser();
+  const router = useRouter();
   const name = user.name == '' ? 'Guest' : user.name;
+
+  const onClickLinkPoker = () => {
+    router.push('/');
+  };
+
   return (
     <div className='flex flex-row'>
       <div className='h-[3vh] px-5 flex justify-center items-center bg-stone-600'>
-        <p className='text-xl'>Link Poker</p>
+        <button className='text-xl' onClick={onClickLinkPoker}>
+          Link Poker
+        </button>
       </div>
       <div className='h-[3vh] px-5 flex justify-center items-center bg-stone-500'>
         <p className='text-xl'>{name}</p>
