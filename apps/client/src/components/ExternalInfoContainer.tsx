6d3d8038429ef422c92c40e3b3847bda8453ff4b
retocrooman <shihou0809@icloud.com>
2024-03-21 00:04:03 +0900

    fix: from Link Poker to LinkPoker

--- apps/client/src/components/ExternalInfoContainer.tsx
+++ apps/client/src/components/ExternalInfoContainer.tsx
@@ -14,7 +14,7 @@ export default function ExternalInfoContainer() {
     <div className='flex flex-row'>
       <div className='h-[3vh] px-5 flex justify-center items-center bg-stone-600'>
         <button className='text-xl' onClick={onClickLinkPoker}>
-          Link Poker
+          LinkPoker
         </button>
       </div>
       <div className='h-[3vh] px-5 flex justify-center items-center bg-stone-500'>
