4acd022555ed5a1b8d900be0690ed35f8b627138
retocrooman <shihou0809@icloud.com>
2024-03-22 23:13:41 +0900

    fix: table log scroll bug

--- apps/client/src/components/TableLogCard.tsx
+++ apps/client/src/components/TableLogCard.tsx
@@ -11,12 +11,14 @@ export default function TableLogCard() {
   }, [tableLogs]);
 
   return (
-    <div className='h-[20vh] w-[40vw] border rounded p-2 overflow-auto' id='table-log-card'>
-      <div>Table Log</div>
-      <div className='flex flex-col justify-start text-sm h-full'>
-        {tableLogs.map((log, index) => (
-          <div key={index}>{log.comment}</div>
-        ))}
+    <div>
+      <div className='text-base bg-white text-stone-800 px-2'>Table Log</div>
+      <div className='h-[18vh] w-[40vw] border rounded p-2'>
+        <div className='flex flex-col justify-start text-sm h-full overflow-auto' id='table-log-card'>
+          {tableLogs.map((log, index) => (
+            <div key={index}>{log.comment}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
